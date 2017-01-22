230cfb77530571cda52281aed2e33cdbdbbbd6f9
S <shanglunwang@gmail.com>
2017-01-22 12:34:00 -0500

    Code consolidation, demo, change article URL

--- src/main/java/seanwang/App.java
+++ src/main/java/seanwang/App.java
@@ -6,13 +6,18 @@ package seanwang;
  */
 public class App 
 {
-    public static void main( String[] args ) throws java.io.IOException, org.xml.sax.SAXException, de.l3s.boilerpipe.BoilerpipeProcessingException
+    public static void main( String[] args ) throws
+            java.io.IOException,
+            org.xml.sax.SAXException,
+            de.l3s.boilerpipe.BoilerpipeProcessingException
     {
-        String urlString = "http://www.cnbc.com/2017/01/15/luxottica-essilor-in-45-billion-euro-merger-deal-to-create-eyewear-giant-source.html";
-        String text = BoilerPipeExtractor.extractFromUrl(urlString);
-        //System.out.println(text);
-        String tagged = PosTagger.tagPos(text);
-        PropNounExtractor.extractProperNouns(tagged);
-        //System.out.println(tagged);
+        PortfolioNewsAnalyzer analyzer = new PortfolioNewsAnalyzer();
+        analyzer.addPortfolioCompany("Luxottica");
+        boolean mentioned = analyzer.analyzeArticle("http://www.reuters.com/article/us-essilor-m-a-luxottica-group-idUSKBN14Z110");
+        if (mentioned) {
+            System.out.println("Article mentions portfolio companies");
+        } else {
+            System.out.println("Article does not mention portfolio companies");
+        }
     }
 }
--- src/main/java/seanwang/PortfolioNewsAnalyzer.java
+++ src/main/java/seanwang/PortfolioNewsAnalyzer.java
@@ -1,23 +1,82 @@
 package seanwang;
 
-import org.apache.xpath.operations.String;
+import com.sun.deploy.util.StringUtils;
 
-import java.util.Collection;
-import java.util.Collections;
-import java.util.HashSet;
+import java.util.*;
+import java.lang.*;
+import java.net.URL;
 
-/**
- * Created by shang on 1/21/2017.
- */
+import edu.stanford.nlp.tagger.maxent.MaxentTagger;
+import de.l3s.boilerpipe.document.TextDocument;
+import de.l3s.boilerpipe.extractors.CommonExtractors;
+import de.l3s.boilerpipe.sax.BoilerpipeSAXInput;
+import de.l3s.boilerpipe.sax.HTMLDocument;
+import de.l3s.boilerpipe.sax.HTMLFetcher;
+
 public class PortfolioNewsAnalyzer {
     private HashSet<String> portfolio;
+    private static final String modelPath = "edu\\stanford\\nlp\\models\\pos-tagger\\english-left3words\\english-left3words-distsim.tagger";
+    private static MaxentTagger tagger;
     public PortfolioNewsAnalyzer(){
         this.portfolio = new HashSet<String>();
+        this.tagger = new MaxentTagger(modelPath);
     }
+    
     public void addPortfolioCompany(String company) {
         this.portfolio.add(company);
     }
+
+    public boolean analyzeArticle(String urlString) throws
+            java.io.IOException,
+            org.xml.sax.SAXException,
+            de.l3s.boilerpipe.BoilerpipeProcessingException
+    {
+        String articleText = this.extractFromUrl(urlString);
+        String tagged = this.tagPos(articleText);
+        HashSet<String> properNounsSet = this.extractProperNouns(tagged);
+        boolean mentioned = this.arePortfolioCompaniesMentioned(properNounsSet);
+        return mentioned;
+    }
+
     public boolean arePortfolioCompaniesMentioned(HashSet<String> articleProperNouns){
         return !Collections.disjoint(articleProperNouns, this.portfolio);
+    }
+    
+    public static HashSet<String> extractProperNouns(String taggedOutput) {
+        HashSet<String> propNounSet = new HashSet<String>();
+        String[] split = taggedOutput.split(" ");
+        List<String> propNounList = new ArrayList<String>();
+        for (String token: split ){
+            String[] split_token = token.split("_");
+            if(split_token[1].equals("NNP")){
+                propNounSet.add(split_token[0]);
+                propNounList.add(split_token[0]);
+            } else {
+                if (!propNounList.isEmpty()) {
+                    propNounSet.add(StringUtils.join(propNounList, " "));
+                    propNounList.clear();
+                }
+            }
+        }
+        if (!propNounList.isEmpty()) {
+            propNounSet.add(StringUtils.join(propNounList, " "));
+            propNounList.clear();
+        }
+        return propNounSet;
+    }
+
+    public static String tagPos(String input) {
+        String output = tagger.tagString(input);
+        return output;
+    }
+
+    public static String extractFromUrl(String userUrl) throws
+            java.io.IOException,
+            org.xml.sax.SAXException,
+            de.l3s.boilerpipe.BoilerpipeProcessingException  {
+        final HTMLDocument htmlDoc = HTMLFetcher.fetch(new URL(userUrl));
+        final TextDocument doc = new BoilerpipeSAXInput(htmlDoc.toInputSource()).getTextDocument();
+        String content = CommonExtractors.ARTICLE_EXTRACTOR.getText(doc);
+        return content;
     }
 }
